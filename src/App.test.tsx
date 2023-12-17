97147296d272dcb35702e5b0725289149f3c6570
Juan Noguerol Tirado <juanogtir@alum.us.es>
2023-12-17 14:46:53 +0100

    tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,14 +5,13 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import TopNav from './components/TopNav/TopNav';
 import store from './redux/store';
 describe('Pruebas para App', () => {
-  // Crea un store de prueba
   beforeEach(() => {});
 
   afterEach(() => {
     // Limpia el DOM después de cada prueba
     cleanup();
   });
-  // Envuelve tu componente con el Provider en tus pruebas
+
   test('TopNav se renderiza con Redux', () => {
     render(
       <Provider store={store}>
