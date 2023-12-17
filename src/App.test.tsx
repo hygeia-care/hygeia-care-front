eff29580d56d64f9cc5d54c3640d64b8aa06e02d
Juan Noguerol Tirado <juanogtir@alum.us.es>
2023-12-17 14:44:13 +0100

    fix app tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,25 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { cleanup, render } from '@testing-library/react';
 import React from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+import TopNav from './components/TopNav/TopNav';
+import store from './redux/store';
+describe('Pruebas para App', () => {
+  // Crea un store de prueba
+  beforeEach(() => {});
 
-
+  afterEach(() => {
+    // Limpia el DOM después de cada prueba
+    cleanup();
+  });
+  // Envuelve tu componente con el Provider en tus pruebas
+  test('TopNav se renderiza con Redux', () => {
+    render(
+      <Provider store={store}>
+        <Router>
+          <TopNav />
+        </Router>
+      </Provider>
+    );
+  });
+});
