4ec4a8e35320bb19fe6eb1b39f2ebfd983805d4b
Juan Noguerol Tirado <juanogtir@alum.us.es>
2024-01-12 00:38:25 +0100

    tests

--- cypress/e2e/Analysis/AnalysisSummary.cy.js
+++ cypress/e2e/Analysis/AnalysisSummary.cy.js
@@ -5,18 +5,18 @@ describe('Pruebas para AnalysisSummary', () => {
   });
 
   it('se renderiza sin errores', () => {
-    cy.getTopNavItemByText('My data').click();
+    goToMyData();
     cy.get('.analysis-summary').should('exist');
   });
 
   it('muestra datos de análisis en la tabla', () => {
-    cy.getTopNavItemByText('My data').click();
+    goToMyData();
     cy.get('[data-testid="cell-value-0"]').should('contain', '10');
     cy.get('[data-testid="cell-value-1"]').should('contain', '20');
   });
 
   it('abrir y cerrar el modal de detalles', () => {
-    cy.getTopNavItemByText('My data').click();
+    goToMyData();
     cy.get('[data-testid="cell-action-0"]').within(() => {
       cy.get('button').click();
     });
@@ -27,9 +27,23 @@ describe('Pruebas para AnalysisSummary', () => {
   });
 
   it('formato de fecha correcto en la tabla', () => {
-    cy.getTopNavItemByText('My data').click();
+    goToMyData();
     cy.get('[data-testid="cell-measurementDate-0"]')
       .invoke('text')
       .should('match', /\d{2}\/\d{2}\/\d{4} \d{2}:\d{2}/);
   });
 });
+
+function mockUserData() {
+  cy.fixture('userData.json').then((response) => {
+    cy.intercept('POST', '/api/v1/auth/users/*', {
+      statusCode: 200,
+      body: response,
+    }).as('userData');
+  });
+}
+
+function goToMyData() {
+  mockUserData();
+  cy.getTopNavItemByText('My data').click();
+}
