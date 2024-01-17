6a5ea98b4753868bd59d9454443ba3557ba959e3
Celia Sánchez Gaitán <celia@MacBook-de-Celia.local>
2024-01-17 23:04:31 +0100

    Mejora cypress appointments

--- cypress/e2e/MyAppointments/DeleteAppointments.cy.js
+++ cypress/e2e/MyAppointments/DeleteAppointments.cy.js
@@ -1,4 +1,3 @@
-
 describe('Pruebas para eliminar Appointment', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000');
@@ -6,20 +5,41 @@ describe('Pruebas para eliminar Appointment', () => {
     cy.visit('http://localhost:3000/appointments');
   });
 
-  it('Comprobar que estamos en mis citas', () => {
+  it('Acceder a mis citas', () => {
     cy.get('h1').should('contain', 'MIS CITAS');
   });
 
+  it('Eliminar una cita y verificar la eliminación', () => {
+    cy.wait(2000);
 
-  it('Eliminar una cita si hay citas programadas', () => {
-  
-    cy.get('.p-button-danger[data-pc-name="button"][data-pc-section="root"]').first().click();
-  
-    cy.window().then(win => {
-      cy.stub(win, 'confirm').returns(true);
+    cy.get('.p-datatable-tbody').children().its('length').then((size) => {
+      if (size > 0) {
+        cy.get('.p-button-danger[data-pc-name="button"][data-pc-section="root"]').first().click();
+
+        // Confirmar la eliminación en una ventana de diálogo
+        cy.window().then(win => {
+          cy.stub(win, 'confirm').returns(true);
+        });
+
+        cy.wait(2000);
+
+        // Recargar la página para reflejar los cambios
+        cy.reload();
+
+        // Esperar que la página se recargue completamente
+        cy.wait(2000);
+
+        if (size === 1) {
+          // Verificar que aparezca el mensaje de "No hay citas programadas"
+          cy.get('.table-container').should('contain', 'No hay citas programadas.');
+        } else {
+          // Verificar que el número de citas ha disminuido
+          cy.get('.p-datatable-tbody').children().should('have.length', size - 1);
+        }
+      } else {
+        // No hay citas para eliminar
+        cy.log('No hay citas disponibles para eliminar');
+      }
     });
-  
   });
-  
 });
-
