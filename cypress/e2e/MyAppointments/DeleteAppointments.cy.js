12d24a91c5ec51236f8ee563725302b2d6fe7d5b
Celia Sánchez Gaitán <celia@MacBook-de-Celia.local>
2024-01-20 16:25:39 +0100

    Cypress DeleteAppointment arreglado

--- cypress/e2e/MyAppointments/DeleteAppointments.cy.js
+++ cypress/e2e/MyAppointments/DeleteAppointments.cy.js
@@ -1,21 +1,23 @@
-/*
+
 describe('Pruebas para Appointment Management', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000');
     cy.login();
-    
   });
 
 
   it('Accede a mis citas', () => {
     goToMyAppointments();
+    
     cy.get('h1').should('contain', 'MIS CITAS');
   });
 
   it('Elimina una cita', () => {
     
+    mockDeleteAppointment()
     goToMyAppointments();
-    cy.get('button .pi-trash').first().click();
+    cy.get('button .pi-trash').first().click({ force: true });
+
     cy.window().then(win => {
       cy.stub(win, 'confirm').returns(true);
     });
@@ -23,19 +25,30 @@ describe('Pruebas para Appointment Management', () => {
 
   it('Verifica la eliminación', () => {
     goToMyAppointments();
-    cy.get('.table-container').should('not.contain', 'Asunto de la cita');
+    cy.get('.table-container').should('not.contain', 'Cita otorrino');
   });
 });
 
 
 function mockAppointmentsData() {
   cy.fixture('appointmentData.json').then((response) => {
-    cy.intercept('GET', '/api/v1/appointments/patients/*', { 
-      statusCode: 200,
-      body: response,
+    cy.intercept('GET', '/api/v1/appointments/patients/*', (req) => {
+      // Registro de la solicitud
+      console.log('Solicitud GET realizada:', req);
+
+      req.reply({
+        statusCode: 200,
+        body: response,
+      });
+
+      // Registro de la respuesta
+      req.on('response', (res) => {
+        console.log('Respuesta recibida:', res);
+      });
     }).as('appointmentData');
   });
 }
+
 
 function goToMyAppointments() {
   mockAppointmentsData();
@@ -44,7 +57,20 @@ function goToMyAppointments() {
   cy.get('.table-container').should('be.visible'); 
 }
 
-*/
+function mockDeleteAppointment() {
+  cy.intercept('DELETE', '/api/v1/appointments/date/*', (req) => {
+
+    req.reply({
+      statusCode: 200, 
+      body: 'Cita eliminada exitosamente' 
+    });
+
+    console.log('Solicitud DELETE interceptada:', req);
+  }).as('deleteAppointment');
+}
+
+
+
 
 
 
